f3561b09d24dabea8326290b065626e4f5bea9f6
David Neumann <david.neumann@student.uni-halle.de>
2025-12-01 10:05:39 +0100

    initial commit

---
+++ src/day1/mod.rs
@@ -0,0 +1,96 @@
+use std::ops::{Add, AddAssign};
+
+#[derive(Copy, Clone)]
+enum Action {
+    Left(u64),
+    Right(u64)
+}
+
+struct Dial(u64);
+
+impl Dial{
+    const fn new() -> Dial{
+        Dial(50)
+    }
+
+    fn check_for_0(&self) -> bool{
+        self.0 == 0
+    }
+}
+
+impl Add<Action> for Dial {
+    type Output = Dial;
+
+    fn add(self, rhs: Action) -> Self::Output {
+        match rhs {
+            Action::Left(neg) => {
+                Dial((100 + self.0 - neg) % 100)
+            },
+            Action::Right(add) => {
+                Dial((self.0 + add) % 100)
+            }
+        }
+    }
+}
+
+impl AddAssign<&Action> for Dial {
+    fn add_assign(&mut self, rhs: &Action) {
+        match rhs {
+            Action::Left(neg) => {
+                self.0 = (100 + self.0 - neg % 100) % 100;
+            },
+            Action::Right(add) => {
+                self.0 = (self.0 + add) % 100;
+            }
+        }
+    }
+}
+
+
+pub(crate) fn day1(lines: &Vec<&str>) -> (u64, u64) {
+    let actions = lines.iter().map(
+        |s| {
+            let (d, n) = s.split_at(1);
+            let n: u64 = n.parse().expect("Couldn't parse number in Day1 Input");
+            match d {
+                "L" => Action::Left(n),
+                "R" => Action::Right(n),
+                _ => panic!("Encountered unexpected character in Day1 Input")
+            }
+        }
+    ).collect();
+    (part1(&actions), part2(&actions))
+}
+
+fn part1(actions: &Vec<Action>) -> u64 {
+    let mut dial = Dial::new();
+    let mut zero_count: u64 = 0;
+
+    for action in actions {
+        dial += action;
+        if dial.check_for_0() {zero_count += 1;}
+    }
+
+    zero_count
+}
+
+fn part2(actions: &Vec<Action>) -> u64 {
+    let mut dial = Dial::new();
+    let mut passes_count: u64 = 0;
+
+    for action in actions {
+        passes_count += match action {
+            Action::Right(pos) => {
+                (dial.0 + pos)/100
+            },
+            Action::Left(neg) => {
+                if dial.0 <= *neg {
+                    (neg - dial.0)/100 + if dial.0 != 0 {1} else {0}
+                } else {0}
+            }
+        };
+        dial += action;
+    }
+
+    passes_count
+}
---
+++ src/main.rs
@@ -0,0 +1,52 @@
+mod day1;
+
+use day1::day1;
+use std::ffi::OsStr;
+use std::fs::File;
+use std::io::{BufReader, Read};
+use std::sync::LazyLock;
+use std::time::Instant;
+
+struct Paths{
+    day1: &'static OsStr,
+    /*day2: &'static OsStr,
+    day3: &'static OsStr,
+    day4: &'static OsStr,
+    day5: &'static OsStr,
+    day6: &'static OsStr,
+    day7: &'static OsStr,
+    day8: &'static OsStr,
+    day17: &'static OsStr,*/
+}
+
+static PATHS: LazyLock<Paths> = LazyLock::new(|| Paths{
+    day1: OsStr::new(r"data\day1\input.txt"),
+    /*day2: OsStr::new(r"data/day2/input.txt"),
+    day3: OsStr::new("data/day3/input.txt"),
+    day4: OsStr::new(r"data/day4/input.txt"),
+    day5: OsStr::new(r"data/day5/input.txt"),
+    day6: OsStr::new(r"data/day6/input.txt"),
+    day7: OsStr::new(r"data/day7/input.txt"),
+    day8: OsStr::new(r"data/day8/input.txt"),
+    day17: OsStr::new(r"data/day17/input.txt"),*/
+});
+
+fn main() {
+    let start = Instant::now();
+    let input = get_input(PATHS.day1);
+    //test_day17();
+    let lines = get_lines(&input);
+    let res = day1(&lines);
+    let duration = start.elapsed();
+    println!("{:?}, Duration: {:?}", res, duration)
+}
+
+fn get_input(path_string: &OsStr) -> String{
+    let mut ret_string = String::new();
+    BufReader::new(File::open(path_string).unwrap()).read_to_string(&mut ret_string).unwrap();
+    ret_string
+}
+
+fn get_lines(string: &str) -> Vec<&str>{
+    string.lines().collect()
+}
