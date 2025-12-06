ca07a37fe0b36fff6910d678dbd4f908d584b4dd
David Neumann <david.neumann@student.uni-halle.de>
2025-12-06 23:08:28 +0100

    implemented day4 solution

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
-use day1::day1;
+/*use day1::day1;
 use day2::day2;
-use day3::day3;
+use day3::day3;*/
+use day4::day4;
 use std::ffi::OsStr;
 use std::fs::File;
 use std::io::{BufReader, Read};
@@ -15,8 +17,8 @@ struct Paths{
     day1: &'static OsStr,
     day2: &'static OsStr,
     day3: &'static OsStr,
-    /*day4: &'static OsStr,
-    day5: &'static OsStr,
+    day4: &'static OsStr,
+    /*day5: &'static OsStr,
     day6: &'static OsStr,
     day7: &'static OsStr,
     day8: &'static OsStr,
@@ -27,8 +29,8 @@ static PATHS: LazyLock<Paths> = LazyLock::new(|| Paths{
     day1: OsStr::new(r"data/day1/input.txt"),
     day2: OsStr::new(r"data/day2/input.txt"),
     day3: OsStr::new("data/day3/input.txt"),
-    /*day4: OsStr::new(r"data/day4/input.txt"),
-    day5: OsStr::new(r"data/day5/input.txt"),
+    day4: OsStr::new(r"data/day4/input.txt"),
+    /*day5: OsStr::new(r"data/day5/input.txt"),
     day6: OsStr::new(r"data/day6/input.txt"),
     day7: OsStr::new(r"data/day7/input.txt"),
     day8: OsStr::new(r"data/day8/input.txt"),
@@ -37,9 +39,9 @@ static PATHS: LazyLock<Paths> = LazyLock::new(|| Paths{
 
 fn main() {
     let start = Instant::now();
-    let input = get_input(PATHS.day3);
+    let input = get_input(PATHS.day4);
     let lines = get_lines(&input);
-    let res = day3(&lines);
+    let res = day4(&lines);
     let duration = start.elapsed();
     println!("{:?}, Duration: {:?}", res, duration)
 }
