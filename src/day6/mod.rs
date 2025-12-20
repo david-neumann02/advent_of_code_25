395d2555f00b217c162eab7d2affec0c64c437a1
David Neumann <davidneumann2155@gmail.com>
2025-12-20 15:04:16 +0100

    implemented most parts of day 6 part 2 solution

--- src/day6/mod.rs
+++ src/day6/mod.rs
@@ -1,3 +1,8 @@
+use core::num;
+use std::ops::Range;
+
+use itertools::Itertools;
+
 pub(crate) fn day6(input: &Vec<&str>) -> (u64, u64) {
     (part1(input), part2(input))
 }
@@ -33,14 +38,40 @@ fn part1(input: &Vec<&str>) -> u64 {
 }
 
 fn part2(input: &Vec<&str>) -> u64 {
-    let operation_vec: Vec<Operation> = input.last().unwrap().split_whitespace()
-    .map(|s|
-        match s {
-            "+" => Operation::Addition,
-            "*" => Operation::Multiplication,
-            _ => panic!("Unknown Operation")
-        }
-    ).collect();
+    let mut number_regions = Vec::new();
+    let mut operation_vec = Vec::new();
 
-    todo!()
+    let mut last_index = 0;
+    for (i, c) in input.last().unwrap().chars().enumerate().skip(1) {
+        if c != ' ' {
+            number_regions.push(last_index..i - 1);
+            last_index = i;
+            operation_vec.push(match c {
+                '+' => Operation::Addition,
+                '*' => Operation::Multiplication,
+                _ => panic!("Unknwon symbol")
+            });
+        }
+    }
+
+    let digit_count: u32 = (input.len() - 1) as u32;
+
+    let range_mapper = |r: Range<usize>| -> Vec<u64> {
+        r.map(|i| 
+            input.iter().take(input.len() - 1).enumerate()
+                .map(|(j, n)| 
+                    n.chars().nth(i).unwrap()
+                        .to_digit(10).unwrap_or(0) as u64 * 10u64.pow(digit_count - (j + 1) as u32)
+                ).sum()
+        ).collect()
+    };
+
+    let num_vecs: Vec<Vec<u64>> = number_regions.into_iter()
+        .map(range_mapper).collect();
+
+    num_vecs.iter().zip(operation_vec.iter()).map(|(v, o)| match o {
+        Operation::Addition => v.iter().sum::<u64>(),
+        Operation::Multiplication => v.iter().product()
+    }).sum()
+
 }
